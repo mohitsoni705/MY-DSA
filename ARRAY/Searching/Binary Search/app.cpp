9a319dd69bb8ad12079af86b1c7ec2d88b8b5794
Mohit Parmar Soni <mohitravet07@gmail.com>
2025-10-24 08:41:00 +0530

    Binary search (recursive)

--- ARRAY/Searching/Binary Search/app.cpp
+++ ARRAY/Searching/Binary Search/app.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
-int binarySearch(vector<int> a, int target){
-   int start = 0 ;
-   int result = -1;
-   int end = a.size() - 1;
-   while(start<=end){
-   int mid = start+(end-start)/2;
-       if(a[mid] == target){
-           result = mid;
-           start = mid+1;
-       }else if(target<a[mid]){
-           end = mid-1;
-       }else{
-           start =  mid+1;
-       }
-   }
-   return result;
+
+int binarySearch(vector<int> numbers , int target){
+    int start = 0;
+    int ans = -1;
+    int end = numbers.size() - 1;
+    while(start <= end){
+        int mid = start + (end - start)/ 2;
+        if(numbers[mid] == target){
+            return mid;
+        }else if(numbers[mid] < target){
+            start = mid + 1;
+        }else{
+            end = mid - 1;
+        }
+    }
+    return ans;
+
 }
-int main() {
-    vector<int> a = {1,2,4,4,4,4,7,8,9};
-    int target = 4;
-    cout<<binarySearch( a , target);
-   
-   
+int main(){
+    vector<int> numbers = {0,1,2,3,4,5,6,7,8,9};
+    int target = 2;
+    cout<<binarySearch(numbers , target)<<endl;
 }
