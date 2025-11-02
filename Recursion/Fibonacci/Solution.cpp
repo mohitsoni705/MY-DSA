1eeda0ee273d29b9e8c62a385172ee19d6250876
Mohit Parmar Soni <mohitravet07@gmail.com>
2025-11-02 05:57:09 +0530

    Recursion - fibonnaci

--- Recursion/Fibonacci/Solution.cpp
+++ Recursion/Fibonacci/Solution.cpp
@@ -2,12 +2,12 @@
 
 using namespace std;
 int func(int n){
-    if(n<=1){
-      return 0;
-    }
+    if(n==0)return 0;
+    if(n==1)return 1;
+
     return func(n-1)+func(n-2);
 }
 int main(){
-    int n = 3;
+    int n = 10;
     cout<<func(n);
 }
